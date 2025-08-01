962f184efbbcaf8c2cb0f21128d12601697c5625
lucas <lucas.smbm.2008@gmail.com>
2025-08-01 00:45:23 -0300

    Add files via upload

---
+++ source/lucas/funkin/engine/Arenanew.hx
@@ -0,0 +1,28 @@
+package lucas.funkin.engine;
+
+import objects.Character;
+
+class Arenanew extends BaseStage
+{
+    var sky:BGSprite;
+    var stands:BGSprite;
+    var railingnew:BGSprite;
+    var ground:BGSprite;
+
+    public function new()
+    {
+        super();
+
+        sky = new BGSprite('matt/Arena/skyBG', -450, -175);
+        add(sky);
+
+        stands = new BGSprite('matt/Arena/standsBG', -450, -225);
+        add(stands);
+
+        railingnew = new BGSprite('matt/Arena/railingBG', -450, -200);
+        add(railingnew);
+
+        ground = new BGSprite('matt/Arena/groundBG', -450, -100);
+        add(ground);
+    }
+}
---
+++ source/lucas/funkin/engine/Castle.hx
@@ -0,0 +1,32 @@
+package lucas.funkin.engine;
+
+import objects.Character;
+import objects.Note;
+
+class Castle extends BaseStage
+{
+    var bg:BGSprite;
+    var wall:BGSprite;
+    var flopor:BGSprite;
+    var darnell:FlxSprite;
+
+    public function new()
+    {
+        super();
+
+        bg = new BGSprite('bowser/bg', -155, -250, 1.2, 0.8);
+        add(bg);
+
+        wall = new BGSprite('bowser/walls', -185, 650, 0.9, 0.9);
+        add(wall);
+
+        flopor = new BGSprite('bowser/main_flopor', 0, 375, 1, 1.0);
+        add(flopor);
+
+        darnell = new FlxSprite(2369, 739);
+        darnell.frames = Paths.getSparrowAtlas('characters/darnell');
+        darnell.animation.addByPrefix("idle", "Idle", 24);
+        darnell.scale.set(1);
+        add(darnell);
+    }
+}
