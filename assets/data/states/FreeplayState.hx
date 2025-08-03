96c39d1884bc0ff507309b88c13b9bd0d57f4ab3
lucas <lucas.smbm.2008@gmail.com>
2025-08-03 12:25:34 -0300

    Add files via upload

---
+++ assets/data/states/FreeplayState.hx
@@ -0,0 +1,15 @@
+import flixel.util.FlxAxes;
+
+var icoPlacement:String = "left";
+
+function postUpdate() {
+    for (p in 0...iconArray.length) {
+        grpSongs.members[p].screenCenter(FlxAxes.X);
+        switch (icoPlacement) {
+            case "right":
+                iconArray[p].x = iconArray[p].sprTracker.x + grpSongs.members[p].width + 10;
+            case "left":
+                iconArray[p].x = iconArray[p].sprTracker.x - grpSongs.members[p].width + iconArray[p].sprTracker.width - 150;
+        }
+    }
+}
