5884b26d31a1c2c11b0b9bf57957224057988f7b
LucasSanches <lucas.smbm.2008@gmail.com>
2025-10-09 18:00:34 -0300

    Coming Soon

--- source/lucas/states/engine/menus/freeplay/meta/FreeplayMeta.hx
+++ source/lucas/states/engine/menus/freeplay/meta/FreeplayMeta.hx
@@ -1,7 +1,7 @@
-package mikolka.funkin.custom;
+package Lucas.funkin.custom;
 
 import haxe.Json;
-using mikolka.funkin.custom.FunkinTools;
+using Lucas.funkin.custom.FunkinTools;
 
 class FreeplayMetaJSON {
     public var freeplayWeekName:String = "";
