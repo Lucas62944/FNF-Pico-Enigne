52d0df5729ca7cc51e9d320675da6a7547b06548
LucasSanches <lucas.smbm.2008@gmail.com>
2025-10-09 17:58:05 -0300

    Add files via upload

---
+++ source/lucas/states/engine/menus/freeplay/meta/FreeplayMeta.hx
@@ -0,0 +1,34 @@
+package mikolka.funkin.custom;
+
+import haxe.Json;
+using mikolka.funkin.custom.FunkinTools;
+
+class FreeplayMetaJSON {
+    public var freeplayWeekName:String = "";
+}
+
+class FreeplayMeta {
+    public static function getMeta(songId:String):FreeplayMetaJSON {
+        var meta_file = Paths.getTextFromFile('data/${Paths.formatToSongPath(songId)}/Meta.json');
+        if(meta_file != null){
+            return getMetaFile(meta_file);
+        }
+        else {
+            return new FreeplayMetaJSON();
+        }
+    }
+    private static function getMetaFile(rawJson:String):FreeplayMetaJSON {
+
+        try {
+            if(rawJson != null && rawJson.length > 0) {
+                return new FreeplayMetaJSON().mergeWithJson(Json.parse(rawJson));
+            }
+        }
+        catch(x){
+            trace("Malfolded json? tf did you do to it?");
+            trace(x.message);
+        }
+		
+		return new FreeplayMetaJSON();
+	}
+}
