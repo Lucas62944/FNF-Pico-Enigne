49349fc0790ca5fb4783655f32c0fa58ba7945fd
Lucas Sanches <lucas.smbm.2008@gmail.com>
2026-01-14 18:12:58 -0300

    Update PicoEngineSubState.hx
    Signed-off-by: Lucas Sanches <lucas.smbm.2008@gmail.com>

--- source/lucas/states/funkin/scripts/options/PicoEngineSubState.hx
+++ source/lucas/states/funkin/scripts/options/PicoEngineSubState.hx
@@ -5,15 +5,14 @@ import options.BaseOptionsMenu;
 
 class PicoEngineSubState extends BaseOptionsMenu {
 	public function new() {
-
     title = Language.getPhrase("vslice_menu","Pico Engine settings (W.I.P)");
     rpcTitle = "Pico-Engine settings menu";
-		{
-
-		var option:Option = new Option('V-Slice Hub (W.I.P)',
-		'If checked, using the HUD for V-Slice.',
-		'hub',
-		BOOL);
+{
+		var option:Option = new Option('Characters Note Skins',
+			"If checked, Enables character-specific noteskins during songs (W.I.P)",
+			'noteskinsCharacters',
+			STRING,
+			['Player','Opponent','Both']);
 		addOption(option);
 		
         var option:Option = new Option('Max Combo (W.I.P)',
@@ -22,13 +21,17 @@ class PicoEngineSubState extends BaseOptionsMenu {
 		BOOL);
 	    addOption(option);
 
+		var option:Option = new Option('V-Slice Hub (W.I.P)',
+		'If checked, using the HUD for V-Slice.',
+		'hub',
+		BOOL);
+		addOption(option);
+
 		var option:Option = new Option('Hold Note (W.I.P)',
 		'Enable/Disable to prevent characters from performing hold animations during music.',
 		'hold',
 		BOOL);
 		addOption(option);
-
 		super();
-		}
 	}
 }
